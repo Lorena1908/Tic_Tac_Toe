aaa93cd61be4dd21ea9f895598174bb15039d656
Lorena <lore.lo2310@gmail.com>
2021-05-24 18:04:06 -0300

    Made the pygame interface

--- main.py
+++ main.py
@@ -1,62 +1,148 @@
 import numpy as np
+import pygame
+from math import floor
+pygame.font.init()
 
 rows, columns = 3, 3
+square = 200
+width = square * rows
+height = square * columns
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Tic Tac Toe')
+font = pygame.font.SysFont('comicsans', 100)
 
 def create_board():
-    # board = [[0 for _ in range(columns)] for _ in range(rows)]
     board = np.zeros((rows, columns))
     return board
 
-def make_tuple(string):
-    return tuple(map(int, string.split(',')))
-
-def winning_move(board, piece):
+def winning_move(board, piece, pos):
     # Horizontal winner
     for row in range(len(board)):
         for column in range(len(board)-2):
             if board[row][column] == piece and board[row][column+1] == piece and board[row][column+2] == piece:
+                pos['start_pos'] = (10, row * square + square/2)
+                pos['end_pos'] = (width-10, row * square + square/2)
                 return True
     
     # Vertical winner
     for row in range(len(board)-2):
         for column in range(len(board)):
             if board[row][column] == piece and board[row+1][column] == piece and board[row+2][column] == piece:
+                pos['start_pos'] = (column * square + square/2, 10)
+                pos['end_pos'] = (column * square + square/2, height-10)
                 return True
     
     # Positively sloped diagonals
     for row in range(len(board)-2):
         for column in range(len(board)-2):
             if board[row][column] == piece and board[row+1][column+1] == piece and board[row+2][column+2] == piece:
+                pos['start_pos'] = (column * square + 20, row * square + 20)
+                pos['end_pos'] = ((column+2) * square + square -20, (row+2) * square + square -20)
                 return True
     
     # Negatively sloped diagonals
     for row in range(2, len(board)):
         for column in range(len(board)-2):
             if board[row][column] == piece and board[row-1][column+1] == piece and board[row-2][column+2] == piece:
+                pos['start_pos'] = (column * square + 20, row * square + square -20)
+                pos['end_pos'] = ((column+2) * square + square -20, (row-2) * square + 20)
                 return True
 
-board = create_board()
+def is_valid_position(board, row, column):
+    return board[row][column] == 0
 
-turn = 0
-print(board)
-run = True
-while run:
-    if turn == 0:
-        print("Player 1's turn")
-        x, y = make_tuple(input('Enter a position: '))
-        board[y][x] = 1
+def draw_grid(board):
+    for row in range(len(board)-1):
+        # Horizontal lines
+        pygame.draw.line(win, (128,128,128), (0, square + row * square), (width, square + row * square), 5)
+        for column in range(len(board)-1):
+            # Vertical lines
+            pygame.draw.line(win, (128,128,128), (square + row * square, 0), (square + row * square, height), 5)
 
-        if winning_move(board, 1):
-            print('Player 1 wins!')
-            run = False
-    else:
-        print("Player 2's turn")
-        x, y = make_tuple(input('Enter a position: '))
-        board[y][x] = 2
+def draw_board(board):
+    for row in range(len(board)):
+        for column in range(len(board)):
+            pygame.draw.rect(win, (0,0,0), (column * square, row * square, square, square))
 
-        if winning_move(board, 2):
-            print('Player 2 wins!')
-            run = False
-    turn += 1
-    turn %= 2
-    print(board)
+    for row in range(len(board)):
+        for column in range(len(board)):
+            x = column * square + square/2
+            y = row * square + square/2
+            if board[row][column] == 1:
+                pygame.draw.line(win, (0,0,255), (x-70, y-70), (x+70, y+70), 25)
+                pygame.draw.line(win, (0,0,255), (x-70, y+70), (x+70, y-70), 25)
+            elif board[row][column] == 2:
+                pygame.draw.circle(win, (255,0,0), (x, y), 80)
+                pygame.draw.circle(win, (0,0,0), (x, y), 60)
+
+def draw_window(board):
+    draw_board(board)
+    draw_grid(board)
+    pygame.display.update()
+
+
+def main():
+    board = create_board()
+    turn = 0
+    pos = {}
+    run = True
+    text = ''
+    draw_window(board)
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                row = int(floor(y/square))
+                column = int(floor(x/square))
+                if turn == 0:
+                    if is_valid_position(board, row, column):
+                        board[row][column] = 1
+
+                        if winning_move(board, 1, pos):
+                            draw_board(board)
+                            text = font.render('Blue Wins!', 1, (255,255,255))
+                            color = (0,0,255)
+                            print('1 wins!')
+                            run = False
+                else:
+                    if is_valid_position(board, row, column):
+                        board[row][column] = 2
+
+                        if winning_move(board, 2, pos):
+                            draw_board(board)
+                            text = font.render('Red Wins!', 1, (255,255,255))
+                            color = (255,0,0)
+                            print('2 wins!')
+                            run = False
+        
+                draw_window(board)
+                turn += 1
+                turn %= 2
+
+                if not run:
+                    print(pos)
+                    pygame.draw.line(win, color, (pos['start_pos'][0], pos['start_pos'][1]), (pos['end_pos'][0], pos['end_pos'][1]), 15)
+                    win.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
+                    pygame.display.update()
+                    pygame.time.wait(2000)
+
+def first_screen():
+    run = True
+
+    while run:
+        win.fill((0,0,0))
+        text = font.render('Click to Play', 1, (255,255,255))
+        win.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                main()
+        pygame.display.update()
+
+first_screen()
